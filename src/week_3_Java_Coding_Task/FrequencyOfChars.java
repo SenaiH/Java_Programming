3a7268a999f0f31bf93e9495f50adbb75409f37c
Senai <simoseni@gmail.com>
2023-06-03 17:46:14 -0700

    Java Programming updated

---
+++ src/week_3_Java_Coding_Task/FrequencyOfChars.java
@@ -0,0 +1,13 @@
+package week_3_Java_Coding_Task;
+
+public class FrequencyOfChars {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+3) String - Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2**
+ */
